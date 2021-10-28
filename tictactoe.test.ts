d3d24920a98b796880d6ade380f3f2aca2bf61bf
Antonio <amp.olivas00@gmail.com>
2021-10-28 00:09:00 -0600

    made changes

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -16,16 +16,35 @@ test('I can start a game', async () => {
 
     let button = await (await driver).findElement(By.id('start-game'));
     await button.click();
-
-    await driver.findElement(By.id('cell-0')).click()
-    
-    await driver.sleep(1000)
-
-    await driver.findElement(By.id('cell-2')).click()
-
-    await driver.sleep(1000)
-    
-    await driver.findElement(By.id('cell-8')).click()
-
-    await driver.sleep(1000)
 });
+
+
+test('Add X to square', async () => {
+     let square = await driver.findElement(By.id('cell-0'));
+    await square.click()
+    
+    expect(await square.getText()).toContain('X')
+    await driver.sleep(2000)
+});
+
+
+    
+test('Add X to square', async () => {
+    let square = await driver.findElement(By.id('cell-2'));
+    await square.click()
+    expect(await square.getText()).toContain('X')
+    await driver.sleep(2000)
+});
+    
+ test('Add X to square', async () => {
+    let square = await driver.findElement(By.id('cell-8'));
+    await square.click()
+    expect(await square.getText()).toContain('X')
+    await driver.sleep(2000)
+});
+
+
+
+
+
+
